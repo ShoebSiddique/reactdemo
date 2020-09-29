a0dbc0e7a3aa0aa1d601067efb7057fa7a2a0a22
ShoebSiddique <shoeb.sas@gmail.com>
2020-09-29 18:41:07 -0500

    added bootstrap

--- rotuing/src/component/Home.js
+++ rotuing/src/component/Home.js
@@ -1,4 +1,5 @@
 import React from "react"
+import 'bootstrap/dist/css/bootstrap.css';
 
 function Home(props) {
     return(<h2> This is Home page.</h2>)
--- rotuing/src/index.js
+++ rotuing/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Routing from './component/Routing';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 
 
